25a18c2d9fdcccf0a80664c1759caf65b2611b58
Peter <37542469+ppak10@users.noreply.github.com>
2018-08-25 17:38:12 -0400

    Introduction to ECS Commit
    Commit sets up basic hybrid ECS scene and adds basic notes

---
+++ Hybrid-ECS-Notes/Assets/Scripts/Rotator.cs
@@ -0,0 +1,44 @@
+﻿// ----------------------------------------------------------------------------
+// Original Creator: Brackey's guy (Asbjørn)
+// File Developer: Peter Pak
+// Description: Script to generate ground mesh from loading terrain chunks
+// ----------------------------------------------------------------------------
+
+using UnityEngine;
+using Unity.Entities;
+
+/// <summary>
+/// MonoBehaviour class used to store DATA
+/// </summary>
+public class Rotator : MonoBehaviour {
+	public float speed; // float to store rotation speed data	
+}
+
+
+/// <summary>
+/// ComponentSystem class used to implement BEHAVIOR which undergoes logic
+/// </summary>
+class RotatorSystem : ComponentSystem
+{
+	/// <summary>
+	/// Struct components to search entities which contain both rotator and transform components
+	/// </summary>
+	struct Components
+	{
+		public Rotator rotator;
+		public Transform transform;
+	}
+
+	/// <summary>
+	/// protected override void which performs updates to entities
+	/// </summary>
+	protected override void OnUpdate()
+	{
+		// search through entities with given component structure
+		foreach (var e in GetEntities<Components>())
+		{
+			// rotate cube according to entity's rotator speed
+			e.transform.Rotate(0f, e.rotator.speed * Time.deltaTime, 0f);
+		}
+	}
+}
